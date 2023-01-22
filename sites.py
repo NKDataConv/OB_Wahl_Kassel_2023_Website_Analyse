979530a5092b983abc10b975fd804f0990963a97
NKDataConv <nicolaskuhaupt@aol.com>
2023-01-22 21:28:46 +0100

    Update sites.py

--- sites.py
+++ sites.py
@@ -1,5 +1,5 @@
-SITES = {"hoermann": "https://www.eva-kuehne-hoermann.de/",
-         "bock": "https://www.violetta-bock.de/",
+SITES = {"Hoermann": "https://www.eva-kuehne-hoermann.de/",
+         "Bock": "https://www.violetta-bock.de/",
          "Carqueville": "https://herzfuerkassel.de/",
          "Schoeller": "https://www.sven-schoeller.de/",
          "Geselle": "https://christian-geselle.de/"}
